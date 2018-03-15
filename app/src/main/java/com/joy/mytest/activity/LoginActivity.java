79a134aa1f2171c49d5e0b4ae6c2b1c81e70e83c
huangyacong <huangyacong@jianbing.com>
2018-03-15 19:16:27 +0800

    11111

--- app/src/main/java/com/joy/mytest/activity/LoginActivity.java
+++ app/src/main/java/com/joy/mytest/activity/LoginActivity.java
@@ -29,6 +29,7 @@ public class LoginActivity extends BaseActivity implements ILoginView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String hah = "jj";
     }
 
     @Override
